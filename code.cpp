06db3aba978a428b8a51f1edb87bf1ee05260276
R B <86495244+ryzenRB@users.noreply.github.com>
2022-09-10 13:25:23 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x,rem;
+	    cin>>x;
+	    
+	    if(x>10){
+	        cout << x%10 << "\n";
+	    }
+	    else{
+	    cout<< x << '\n';
+	}}
+	return 0;
+}
